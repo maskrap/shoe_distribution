98e6ed7e802e9944e04e4bdc3bdd613f482adb76
Sam Park <sampark18@gmail.com>
2016-07-22 11:07:45 -0700

    Add front end index, layout, stores, store, brands, and brand

--- app.rb
+++ app.rb
@@ -6,3 +6,38 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get '/' do
   erb :index
 end
+
+get '/stores' do
+  @stores = Store.all
+  erb :stores
+end
+
+get '/brands' do
+  @brands = Brand.all
+  erb :brands
+end
+
+post '/stores/new' do
+  name = params['name']
+  @store = Store.create({name: name})
+  @brands = Brand.all
+  redirect '/stores'
+end
+
+get '/stores/:id' do
+  @store = Store.find(params["id"].to_i)
+  @brands = Brand.all
+  erb :store
+end
+
+post '/brands/new' do
+  name = params["name"]
+  brand = Brand.create({name: name})
+  redirect '/brands'
+end
+
+get '/brands/:id' do
+  @brand = Brand.find(params["id"].to_i)
+  @stores = Store.all
+  erb :brand
+end
