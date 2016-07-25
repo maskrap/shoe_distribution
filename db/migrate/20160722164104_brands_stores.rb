3a99ecf4d73ec286b716b4ef4409202cc8fad720
Sam Park <sampark18@gmail.com>
2016-07-25 16:01:39 -0700

    Fix migration edit error

--- db/migrate/20160722164104_brands_stores.rb
+++ db/migrate/20160722164104_brands_stores.rb
@@ -1,6 +1,6 @@
 class BrandsStores < ActiveRecord::Migration
   def change
-    create_table(:brands_stores) do |t|
+    create_table(:brand_store) do |t|
       t.column(:brand_id, :integer)
       t.column(:store_id, :integer)
     end
